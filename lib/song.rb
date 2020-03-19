1e8d256ff2f57900c846d7d4f3f755f75cdf405a
C. Leow <chienleow1@gmail.com>
2020-03-19 16:09:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,26 +30,26 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count |genre|
-        genre_count |genre| += 1
+      if genre_count[genre]
+        genre_count[genre] += 1
       else 
-        genre_count |genre| = 1
+        genre_count[genre] = 1
       end
     end
     genre_count
   end
 
-  # def self.artist_count
-  #   artist_count = {}
-  #   @@artists.each do |artist|
-  #     if artist_count |artist|
-  #       artist_count |artist| += 1
-  #     else
-  #       artist_count |artist| = 1
-  #     end
-  #   end
-  #   artist_count
-  # end
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
 end
 
